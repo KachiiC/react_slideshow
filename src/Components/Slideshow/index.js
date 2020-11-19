f76a089e5c1afd53fa8732b767ea558374f82fcb
Kachii <Kacheokcheong@gmail.com>
2020-11-19 15:36:16 +0000

    initial commit

---
+++ src/Components/Slideshow/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+
+const Slideshow = () => {
+
+    return (
+
+        <section className="slideshow">
+            Slideshow
+        </section>
+        
+    )
+}
+
+export default Slideshow
